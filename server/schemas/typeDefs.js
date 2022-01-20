0a2b979c46ebc76e13d8598a0ad5bdded6427b5e
tmantena1 <tmantena1@gmail.com>
2022-01-19 19:59:59 -0500

    added type degs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+	type User {
+		_id: ID
+		username: String
+		email: String
+		password: String
+		savedBooks: [Book]
+	}
+
+	type Book {
+		authors: String
+		description: String
+		bookId: String
+		image: String
+		link: String
+		title: String
+	}
+
+	type Auth {
+		token: ID!
+		user: User
+	}
+
+	type Query {
+		users: [User]
+		user: User
+		books: [Book]
+	}
+
+	type Mutation {
+		addUser(username: String!, email: String!, password: String!): Auth
+		login(email: String!, password: String!): Auth
+		saveBook(
+			authors: String!
+			description: String!
+			bookId: String!
+			link: String!
+			title: String!
+		): User
+		deleteBook(bookId: String!): User
+	}
+`;
+
+module.exports = typeDefs;
